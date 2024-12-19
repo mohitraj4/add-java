afa1f45e94f7bccc75d3df4fd85b9423c62b358b
Mohit raj <80747182+mohitraj4@users.noreply.github.com>
2024-12-19 16:32:14 +0530

    Add files via upload
    array

---
+++ Example2.java
@@ -0,0 +1,12 @@
+package myCodeJava;
+
+public class Example2 {
+	public static void main(String[] args) {
+		int[]arr= {25,56,43,22,45,54};
+		for(int i=1;i<arr.length;i++) {
+			System.out.println("index:"+i+"Value:"+arr[i]);
+		}
+	}
+	
+		
+}
